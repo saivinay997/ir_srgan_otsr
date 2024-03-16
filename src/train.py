e61d7e4527edb6d15e91192e878836ae942086be
saivinay997 <111108274+saivinay997@users.noreply.github.com>
2024-03-16 11:20:58 +0530

    updated

--- src/train.py
+++ src/train.py
@@ -101,7 +101,7 @@ def main(HR_train, HR_val, ymlpath, val_results_path, trained_model_path):
                     # log the images to wandb
                     _lr_img = utils.tensor2img(val_lr)
                     
-                    wb_util.log_image_table(lr_imgs=_lr_img, hr_imgs=gt_img, sr_img=sr_img, step=current_step)
+                    wb_util.log_image_table(lr_imgs=_lr_img, hr_imgs=gt_img, sr_imgs=sr_img, step=current_step)
 
                     # print("Starting to save image.")
                     save_img_path = os.path.join(img_dir, f"{epoch}_{idx}.png")
